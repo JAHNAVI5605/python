273ced92b0f33628f001ade51aa0dc7517f6950d
JAHNAVI5605 <60418755+JAHNAVI5605@users.noreply.github.com>
2020-01-29 10:57:15 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+def add(a=2,b=3):
+    c= a+b
+    print(c)
+add()
+def sub(a=5,b=3):
+    c=a-b
+    print(c)
+sub()
+def mul(a=3,b=4):
+    c=a*b
+    print(c)
+mul()
+def div(a=8,b=2):
+    c=a/b
+    print(c)
+div()
+def mod(a,b):
+    c=a%b
